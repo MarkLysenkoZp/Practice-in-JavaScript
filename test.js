5b16a7fdbaac8ad47f91bd5798348f9e0f867079
MarkLysenkoZp <98688635+MarkLysenkoZp@users.noreply.github.com>
2022-09-15 13:59:43 +0300

    Update test.js

--- test.js
+++ test.js
@@ -24,43 +24,45 @@
 
   function mathsUpdate(a,b, action = 'add'){
 
-    switch(action) {
-      case 'add':
-      return a+b
-      break;
+   switch(action){
+     case 'add':
+     return a+b
+     break;
 
-      case 'mult':
+     case 'mult':
       return a*b;
       break;
 
-      case 'min':
-      return a-b;
+     case 'min':
+     return a-b;
+     break;
+
+     case 'div':
+     return a/b;
       break;
-
-      case 'div':
-        return a/b;
-        break;
+     default:
+     throw new Error('Arithmetic operation not found');
     }
-  
   }
-  mathsUpdate(6,2,"add")
-
- function hash_Maths(a,b, action = 'add'){
-    const maths = { 
+  mathsUpdate(6,2,"add");
+ 
+    
+  function hash_Maths(a,b, action = 'add'){
+   const maths = { 
      add: function() {return a + b},
      mult: function() {return a * b},
      min: function() {return a - b},
      div: function() {return a / b},
     }; 
 
-     const result  = Object.keys(maths);
-     const isFound = result.includes(action);
+    const result  = Object.keys(maths);
+    const isFound = result.includes(action);
 
-     if(isFound == false){
-       throw new Error('Arithmetic operation not found');
-     }          
+   if(isFound == false){
+    throw new Error('Arithmetic operation not found');
+   }          
 
-     const operation = maths[action];
-     return  operation()   
-}
-hash_Maths(6,2,'add');  
+    const operation = maths[action];
+    return  operation()   
+  }
+  hash_Maths(6,2,'add');  
