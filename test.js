e5f44015e7a7871705c527343c62bb1e0045a8f9
MarkLysenkoZp <98688635+MarkLysenkoZp@users.noreply.github.com>
2022-09-15 14:13:59 +0300

    Update test.js

--- test.js
+++ test.js
@@ -17,50 +17,49 @@
     } 
     else{
       throw new Error('Arithmetic operation not found');
-    }
-      
+    } 
   }
   maths(6,2,"add");
 
   function mathsUpdate(a,b, action = 'add'){
 
-   switch(action){
-     case 'add':
-     return a+b
-     break;
+    switch(action){
+      case 'add':
+      return a+b
+      break;
 
-     case 'mult':
+      case 'mult':
       return a*b;
       break;
 
-     case 'min':
-     return a-b;
-     break;
-
-     case 'div':
-     return a/b;
+      case 'min':
+      return a-b;
       break;
-     default:
-     throw new Error('Arithmetic operation not found');
+
+      case 'div':
+      return a/b;
+      break;
+      default:
+      throw new Error('Arithmetic operation not found');
     }
   }
   mathsUpdate(6,2,"add");
  
     
   function hash_Maths(a,b, action = 'add'){
-   const maths = { 
-     add: function() {return a + b},
-     mult: function() {return a * b},
-     min: function() {return a - b},
-     div: function() {return a / b},
+    const maths = { 
+      add: function() {return a + b},
+      mult: function() {return a * b},
+      min: function() {return a - b},
+      div: function() {return a / b},
     }; 
 
     const result  = Object.keys(maths);
     const isFound = result.includes(action);
 
-   if(isFound == false){
-    throw new Error('Arithmetic operation not found');
-   }          
+    if(isFound == false){
+     throw new Error('Arithmetic operation not found');
+    }          
 
     const operation = maths[action];
     return  operation()   
